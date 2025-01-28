ff921a2e0c4a11922f8ed8ecc623549e990c6575
Anoop Chemencherry <anoopchemencherry@Silver-Spider.local>
2025-01-28 16:41:41 +1100

    Initial commit

---
+++ app/src/main/java/com/anoop/vitae/MainActivity.kt
@@ -0,0 +1,50 @@
+package com.anoop.vitae
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.anoop.vitae.ui.theme.VitaeTheme
+
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            VitaeTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    MainNavigation()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    VitaeTheme {
+        Greeting("Android")
+    }
+}
---
+++ app/src/main/java/com/anoop/vitae/ui/screens/ResumeScreen.kt
@@ -0,0 +1,10 @@
+package com.anoop.vitae.ui.screens
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun ResumeScreen(
+    modifier: Modifier
+) {
+}
